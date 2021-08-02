a26dac45ccab2af7c681e58b821bd300b320de60
zeynab <zeynab.rostami76@gmail.com>
2021-08-02 14:34:04 +0430

    Add project files.

---
+++ MyProfile/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProfile.Models
+{
+    public class Contact
+    {
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Github { get; set; }
+    }
+}
---
+++ MyProfile/Models/Protfolio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProfile.Models
+{
+    public class Protfolio
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
