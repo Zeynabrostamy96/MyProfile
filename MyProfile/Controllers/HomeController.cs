5111aa8622b11d42161db2781ce7bdebe46acc3d
zeynab <zeynab.rostami76@gmail.com>
2021-08-03 12:39:33 +0430

    index & contact page added

--- MyProfile/Controllers/HomeController.cs
+++ MyProfile/Controllers/HomeController.cs
@@ -1,37 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyProfile.Models;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace MyProfile.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<Protfolio> _portfolios = new List<Protfolio>
-        {
-            new Protfolio
-            {
-                Id = 1, Title = "KATANA", Image = "01.jpg", Description = "این اولین پروژه من بود!!!"
-            },
-            new Protfolio
-            {
-                Id = 2, Title = "پیام رسان", Image = "02.jpg", Description = "پیام رسان اختصاصی گروه برنامه نویسان"
-            },
-            new Protfolio
-            {
-                Id = 3, Title = "شبکه اجتماعی", Image = "03.jpg",
-                Description = "شبکه اجتماعی ملی در راستای حمایت از تولید ملی"
-            },
-            new Protfolio
-            {
-                Id = 4, Title = "درخواست غذا", Image = "04.jpg",
-                Description = "اپلیکیشن درخواست آنلاین غذای نیمه آماده و آماده طبخ"
-            }
-        };
+        
 
         public IActionResult Index()
         {
@@ -40,19 +16,14 @@ namespace MyProfile.Controllers
             ViewBag.headingText =
                 "این سایت به جهت معرفی من به مشتریان و کسانی که می خواهند از خدمات سایت بهره مند شوند طراحی و پیاده سازی شده است";
          
-            return View(_portfolios);
+            return View();
             
            
         }
         public IActionResult Contact()
         {
-            var contact = new Contact()
-            {
-                Email = "hossein.abbasabadi@outlook.com",
-                Mobile = "09128099518",
-                Github = "github.com/atriya"
-            };
-            return View(contact);
+           
+            return View();
         }
       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
