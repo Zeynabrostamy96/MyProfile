2957b91b9afa15092533dceca56efb9dabbcb8d9
zeynab <zeynab.rostami76@gmail.com>
2021-08-04 14:30:06 +0430

    header move partial view

---
+++ MyProfile/Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProfile.Models
+{
+    public class Project
+    {
+        public int id { get; set; }
+        
+        public string Name { get; set; }
+        public  string Description { get; set; }
+        public string Client { get; set; }
+        public string Image { get; set; }
+        public Project(int Id,string name, string description, string client, string image)
+        {
+            id = Id;
+            Name = name;
+            Description = description;
+            Client = client;
+            Image = image;
+              
+
+        }
+    }
+}
---
+++ MyProfile/ViewComponent/ProjectViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyProfile.Models;
+
+namespace MyProfile.ViewComponentt
+{
+    public class ProjectViewComponent:ViewComponent
+    {
+       
+        public IViewComponentResult Invoke()
+        {
+
+            var projects = new List<Project>
+        {
+         new Project(1,"تاکسی","درخواست انلاین تاکسی برای سفرهای درون شهری ","snap","project-1.jpg"),
+         new Project(2,"زودفو","درخواست انلاین غذا برای سراسرکشور","zoodfood","project-2.jpg"),
+         new Project(3,"مدارس","سیستم مدیریت یکپارچه","parvin","project-3.jpg"),
+         new Project(4,"فضا پیما","سیستم مدیریت ناسا","nasa","project-4.jpg"),
+        };
+            return View("_Project",projects);
+        }
+    }
+}
